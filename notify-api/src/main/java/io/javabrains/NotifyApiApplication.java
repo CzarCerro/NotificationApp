7103df0b67d0cc27f8e8291e6c5cc0420f833799
CzarCerro <abramobligacion@gmail.com>
2022-08-31 18:12:31 +0800

    initialize backend

---
+++ notify-api/src/main/java/io/javabrains/NotifyApiApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyApiApplication.class, args);
+	}
+
+}
